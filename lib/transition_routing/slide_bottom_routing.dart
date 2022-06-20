521dca7166b9bbe8b9967d18e5628648f36654f8
HP Computer s <sohailokzz@gmail.com>
2022-06-20 22:51:33 +0500

    Different routing transition added

---
+++ lib/transition_routing/slide_bottom_routing.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SlideBottomRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideBottomRoute({
+    required this.page,
+  }) : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionDuration: const Duration(milliseconds: 500),
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(0, 1),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          ),
+        );
+}
