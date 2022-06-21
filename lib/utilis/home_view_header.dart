aac7f05d5897b2a5ca8bf136a875e5aaf43b7c27
HP Computer s <sohailokzz@gmail.com>
2022-06-21 23:10:05 +0500

    Home View header has been added

---
+++ lib/utilis/home_view_header.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeViewHeader extends StatelessWidget {
+  const HomeViewHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 20,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome Back',
+                style: GoogleFonts.poppins(
+                  color: const Color(0xFFADA4A5),
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                'Stefani Wong',
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                color: Colors.grey.shade200,
+              ),
+              child: const Center(
+                child: Icon(
+                  Icons.notifications_active_outlined,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../utilis/home_view_header.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Column(
+        children: const [
+          HomeViewHeader(),
+        ],
+      ),
+    );
   }
 }
