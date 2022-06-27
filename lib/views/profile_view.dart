05276cbf7fd53456605c682130fc8c0d4ff0200b
HP Computer s <sohailokzz@gmail.com>
2022-06-27 23:55:10 +0500

    Profile details added

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../utilis/widgets/reusable_header.dart';
 
 class ProfileView extends StatelessWidget {
@@ -25,9 +26,58 @@ class ProfileView extends StatelessWidget {
                       ],
                     ),
                   ),
-                )
+                  child: Image.asset(
+                    'assets/images/profilepic.png',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Stefani Wong',
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        'Lose a Fat Program',
+                        style: GoogleFonts.poppins(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: const Color(0xFF7B6F72),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 28,
+                  width: 68,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: const Color(0xFF92A3FD),
+                      primary: const Color(0xFF92A3FD),
+                      padding: const EdgeInsets.all(0.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Edit',
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
