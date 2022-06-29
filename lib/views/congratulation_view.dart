7bd2d310b8342e46bf3ad5d98420312e3d4e9b65
HP Computer s <sohailokzz@gmail.com>
2022-06-29 23:34:51 +0500

    Congratulation view added

---
+++ lib/views/congratulation_view.dart
@@ -0,0 +1,12 @@
+import 'package:fitness_app/utilis/reusable_button.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CongratulationView extends StatelessWidget {
+  const CongratulationView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
